86c67fc6c10d653a9446251d0fa766f813976931
Calvin Rai <raicalvin@gmail.com>
2018-10-08 18:07:37 -0700

    Updated contract class to include inner class for PetEntry.

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,29 @@
 package com.example.android.pets.data;
 
+import android.provider.BaseColumns;
+
 /* Make this class final so that it can't be extended. We only need it to store the constants. */
 public final class PetContract {
+
+    private PetContract() {}
+
+    public static final class PetEntry implements BaseColumns {
+
+        public final static String TABLE_NAMES = "pets";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME = "name";
+        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_PET_GENDER = "gender";
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+
+
+    }
+
+
 }
